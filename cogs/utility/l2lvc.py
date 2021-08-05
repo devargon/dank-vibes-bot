e284eb20b06ec781c181519c76a07f878fb5b56f
argo0n <laiyeqi@gmail.com>
2021-08-05 14:05:44 +0800

    L2L VC utility

---
+++ cogs/utility/l2lvc.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class L2LVC(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if member.bot:
+            return
+        if member.guild.id != 595457764935991326:
+            return
+        l2lrole = member.guild.get_role(798074726458982420)
+        if before.channel == after.channel:
+            return
+        if after.channel is not None:
+            if after.channel.id == 838437334553985045:
+                await member.add_roles(l2lrole, reason=f"{member} joined the Last to Leave VC")
+            elif before.channel is not None and before.channel.id == 838437334553985045:
+                await member.remove_roles(l2lrole, reason=f"{member} left the Last to Leave VC")
+        elif before.channel is not None and before.channel.id == 838437334553985045:
+            await member.remove_roles(l2lrole, reason=f"{member} left the Last to Leave VC")
