e17ed3f1bbab1aa7bf11310d9d0cab97636728b2
argo0n <laiyeqi@gmail.com>
2021-09-07 18:03:59 +0800

    add message reset command

--- cogs/admin/admin.py
+++ cogs/admin/admin.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from discord.ext import commands
 from .serverrule import ServerRule
 from .sticky import Sticky
+from utils import checks
 
 class CompositeMetaClass(type(commands.Cog), type(ABC)):
     pass
@@ -120,3 +121,19 @@ class Admin(Sticky, ServerRule, commands.Cog, name='admin', metaclass=CompositeM
             return await ctx.send(f"No configuration for DM and nickname requests have been set yet. ")
         else:
             await ctx.send(embed=discord.Embed(title=f"Configurations for {ctx.guild.name}", description = f"Nickname requests: {ctx.guild.get_channel(result.get('nicknamechannel_id'))}\nDM requests: {ctx.guild.get_channel(result.get('dmchannel_id'))}", color = 0x57F0F0))
+
+    @checks.has_permissions_or_role(administrator=True)
+    @commands.command(name="messagereset", aliases=["mreset"], invoke_without_command=True)
+    async def messagelog(self, ctx):
+        """
+        Resets the database for counting messages sent.
+        """
+        messagecount = await self.client.pool_pg.fetch("SELECT * FROM messagelog")
+        if len(messagecount) == 0:  # if there's nothing to be deleted
+            return await ctx.send("There's no message count to be removed.")
+        totalvote = sum(userentry.get('messagecount') for userentry in messagecount)
+        embed = discord.Embed(title="Action awaiting confirmation", description=f"There are {len(messagecount)} who have chatted, amounting to a total of {totalvote} messages. Are you sure you want to reset the message count?", color=self.client.embed_color, timestamp=datetime.utcnow())
+        msg = await ctx.reply(embed=embed)
+        await self.client.pool_pg.execute("DELETE FROM messagelog")
+        embed.color, embed.description = discord.Color.green(), "The message count has been cleared."
+        await msg.edit(embed=embed)
