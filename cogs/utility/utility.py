d72920a6660ce0d4a1b459349d0c0e75aabbc2fb
argo0n <laiyeqi@gmail.com>
2021-09-07 18:01:31 +0800

    add messagecount command

--- cogs/utility/utility.py
+++ cogs/utility/utility.py
@@ -10,6 +10,8 @@ from .nicknames import nicknames
 from discord.ext import commands
 from .teleport import Teleport
 from .suggestion import Suggestion
+from utils.format import ordinal
+from utils import checks
 
 class CompositeMetaClass(type(commands.Cog), type(ABC)):
     pass
@@ -148,3 +150,22 @@ class Utility(Whois, L2LVC, nicknames, Suggestion, Teleport, commands.Cog, name=
         )
         embed.set_footer(icon_url=ctx.guild.icon_url, text="uwu") # you can remove this if you want idk
         await ctx.send(embed=embed)
+
+    @checks.in_beta()
+    @commands.command(name="messagecount", aliases=["mymessages"])
+    async def messagecount(self, ctx, member:discord.Member = None):
+        """
+        Shows the number of messages a member has sent in <#608498967474601995>.
+        """
+        if member is None:
+            member = ctx.author
+        user = await self.client.pool_pg.fetchrow("SELECT * FROM messagelog WHERE user_id = $1", member.id)
+        if user is None:
+            return await ctx.send("Hmm... it appears that you have not sent a message in <#608498967474601995>. Contact a mod if you think this is wrong.")
+        all = await self.client.pool_pg.fetch("SELECT * FROM messagelog ORDER BY messagecount DESC")
+        position = ordinal(all.index(user)+1)
+        embed = discord.Embed(title="Your number of messages sent in #general-chat", color=self.client.embed_color, timestamp=datetime.datetime.utcnow())
+        embed.set_author(name=member, icon_url=member.avatar_url)
+        embed.add_field(name="Message count", value=user.get('messagecount'), inline=True)
+        embed.add_field(name="Position", value=f"{position} {'🏆' if all.index(user) < 10 else ''}", inline=True)
+        await ctx.reply(embed=embed)
