76441d57a4eaf4cdd6303e08bc3c203e22452fe3
argo0n <laiyeqi@gmail.com>
2021-09-08 17:06:48 +0800

    update messagetracking

--- cogs/messagetracking/messagetracking.py
+++ cogs/messagetracking/messagetracking.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+import json
+import asyncio
 
-class MessageTracking(commands.Cog, name='mod'):
+class MessageTracking(commands.Cog, name='MessageTracking'):
     def __init__(self, client):
         self.client = client
         self.queue = []
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel in self.queue:
@@ -13,20 +16,18 @@ class MessageTracking(commands.Cog, name='mod'):
             return
         if not message.guild:
             return
-        result = await self.client.pool_pg.fetchrow("SELECT * FROM stickymessages WHERE guild_id = $1 and channel_id = $2", message.guild.id, message.channel.id)
-        if result is None:
+        if message.author.bot:
+            return
+        if message.webhook_id:
+            return
+        if message.channel.id != 871734809653833740:
             return
         self.queue.append(message.channel)
-        try:
-            old_bot_message = await message.channel.fetch_message(result.get('message_id'))
-        except discord.NotFound:
-            pass
+        result = await self.client.pool_pg.fetchrow("SELECT * FROM messagelog WHERE user_id = $1", message.author.id)
+        if result is None:
+            await self.client.pool_pg.execute("INSERT INTO messagelog VALUES($1, $2)", message.author.id, 1)
         else:
-            await old_bot_message.delete()
-        if result.get('type') == 0:
-            embedjson = json.loads(result.get('message'))
-            newmessage = await message.channel.send(embed=discord.Embed.from_dict(embedjson))
-        else:
-            newmessage = await message.channel.send(result.get('message'))
-        await self.client.pool_pg.execute("UPDATE stickymessages SET message_id = $1 WHERE guild_id = $2 and channel_id = $3", newmessage.id, message.guild.id, message.channel.id)
+            existing_count = result.get('messagecount')
+            await self.client.pool_pg.execute("UPDATE messagelog SET messagecount = $1 WHERE user_id = $2", existing_count+1, message.author.id)
+        await asyncio.sleep(8)
         self.queue.remove(message.channel)
