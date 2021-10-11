e853c07cb52010ad7d02732ea6cc590d28c9f8e8
argo0n <laiyeqi@gmail.com>
2021-10-11 08:03:05 +0800

    match new position

--- cogs/fun/imgen.py
+++ cogs/fun/imgen.py
@@ -36,40 +36,7 @@ class imgen(commands.Cog):
             file = discord.File(fp=b, filename="goeatpoop.jpg")
             return file
         file = await loop.run_in_executor(None, generate)
-        await ctx.send("If you don't understand the reference: <https://www.youtube.com/watch?v=M-PvB0NdO2g>",
-                       file=file)
-
-    @checks.has_permissions_or_role(administrator=True)
-    @commands.cooldown(10, 1, commands.BucketType.user)
-    @commands.command(name="goeatpoop")
-    async def goeatpoop(self, ctx, member: discord.Member = None):
-        """
-        Get someone to eat poop
-        """
-        if member is None:
-            return await ctx.send("mention someone lol")
-        loop = asyncio.get_event_loop()
-        ctxavatar = await ctx.author.display_avatar.read()
-        member_avatar = await member.display_avatar.read()
-
-        def generate():
-            main = Image.open("assets/poop.png")
-            ima = Image.open(BytesIO(ctxavatar), formats=['png']).convert('RGBA')
-            ima = ima.resize((220, 220))
-            backg = main.copy()
-            backg.paste(ima, (107, 218), ima)
-            ima2 = Image.open(BytesIO(member_avatar)).convert('RGBA')
-            ima2 = ima2.resize((227, 227))
-            backg.paste(ima2, (555, 112), ima2)
-            b = BytesIO()
-            backg.save(b, format="png", optimize=True, quality=50)
-            b.seek(0)
-            file = discord.File(fp=b, filename="goeatpoop.jpg")
-            return file
-
-        file = await loop.run_in_executor(None, generate)
-        await ctx.send("If you don't understand the reference: <https://www.youtube.com/watch?v=M-PvB0NdO2g>",
-                       file=file)
+        await ctx.send(file=file)
 
     @checks.dev()
     @commands.command(name="stank")
@@ -88,7 +55,7 @@ class imgen(commands.Cog):
             ima2 = ima2.resize((400,400))
             backg.paste(ima2, (545,586), ima2)
             main2 = Image.open("assets/stankbase_finger.png").convert('RGBA')
-            backg.paste(main2, (0,0), main2)
+            backg.paste(main2, (556,281), main2)
             backg = backg.resize((500,500))
             b = BytesIO()
             backg.save(b, format="png", optimize=True, quality=25)
