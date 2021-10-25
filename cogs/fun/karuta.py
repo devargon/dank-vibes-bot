273b9868cebc05c0e64dd1d7fc7b4b5f623309cc
argo0n <laiyeqi@gmail.com>
2021-10-26 07:25:45 +0800

    changes listed below

--- cogs/fun/karuta.py
+++ cogs/fun/karuta.py
@@ -1,4 +1,5 @@
 import asyncio
+import difflib
 import operator
 from utils.format import print_exception, comma_number
 import discord
@@ -8,7 +9,15 @@ import random
 from discord.ext import menus
 from utils import checks
 from utils.menus import CustomMenu
+import math
 
+items = ['skull', 'argonphallicobject']
+
+def get_item_name(name):
+    lst = difflib.get_close_matches(name, items, n=1, cutoff=0.4)
+    if len(lst) == 0:
+        return None
+    return lst[0]
 
 class SkullLeaderboard(menus.ListPageSource):
     def __init__(self, entries, title):
@@ -27,26 +36,14 @@ class karutaevent(discord.ui.View):
 
     def __init__(self, client, emoji_array, correct_emoji):
         self.response = None
-        self.zombieno = 10
+        self.zombieno = 20
         self.pressed_data = {}
         self.wrong_buttons = []
         self.returning_value = None
         super().__init__(timeout=10.0)
 
-        emojis = [("🔨", "⛏️", "🪓",),
-                  ("🙈", "🙉", "🙊",),
-                  ("🎃", "👽", "🤡",),
-                  ("🐟", "🐬", "🐳",),
-                  ("⭕", "🚫", "❌",),
-                  ("🙂", "🙃", "😐",),
-                  ("🌘", "🌒", "🌔",),
-                  ("🃏", "🎴", "🀄",),
-                  ("☘️", "🍀", "🍃",),
-                  ("🌼", "🌻", "🌷",)
-                  ]
-
         async def update_stat(user):
-            if random.choice([False, False, False, False, True]):
+            if random.choice([False, False, False, False, False, True]):
                 random.shuffle(self.children)
                 await self.response.edit(view=self)
             if self.zombieno <= 0:
@@ -70,8 +67,9 @@ class karutaevent(discord.ui.View):
                     self.pressed_data[user] = 1
                 else:
                     self.pressed_data[user] = self.pressed_data[user] + 1
-                embed.description = embed_desc + f"\n\n{self.zombieno * '🧟'}"
-                await self.response.edit(embed=embed)
+                if self.zombieno % 7 == 0:
+                    embed.description = embed_desc + f"\n\n{self.zombieno * '🧟'}"
+                    await self.response.edit(embed=embed)
             if self.zombieno == 0:
                 for b in self.children:
                     b.disabled = True
@@ -82,14 +80,14 @@ class karutaevent(discord.ui.View):
                 self.stop()
         class somebutton(discord.ui.Button):
             async def callback(self, interaction: discord.Interaction):
-                await interaction.response.defer()
+                #await interaction.response.defer()
                 if interaction.user in karutaevent.wrong_buttons:
-                    await interaction.followup.send("You were killed by the zombies and can't futher interact with them.", ephemeral=True)
+                    await interaction.response.send_message("You were killed by the zombies and can't futher interact with them.", ephemeral=True)
                 elif str(self.emoji) == correct_emoji:
                     await update_stat(interaction.user)
                 else:
                     karutaevent.wrong_buttons.append(interaction.user)
-                    await interaction.followup.send("Oh no! You selected the wrong button and were killed by the zombies. :(", ephemeral=True)
+                    await interaction.response.send_message("Oh no! You selected the wrong button and were killed by the zombies. :(", ephemeral=True)
 
         for emoji in emoji_array:
             self.add_item(somebutton(emoji=emoji, style=discord.ButtonStyle.grey))
@@ -128,6 +126,7 @@ class karuta(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # sourcery skip: none-compare, remove-pass-body, remove-redundant-fstring, switch
         try:
             if self.karutaevent_isrunning:
                 return
@@ -135,7 +134,10 @@ class karuta(commands.Cog):
                 self.karutaconfig = await self.client.pool_pg.fetchrow("SELECT * FROM karutaeventconfig")
             if self.karutaconfig is None:
                 return
-            if message.channel.id != self.karutaconfig.get('channel_id'):
+            if self.karutaconfig.get('channel_id') is None or self.karutaconfig.get('channel_id') == '':
+                return
+            whitelisted_channel_ids = self.karutaconfig.get('channel_id').split(',')
+            if str(message.channel.id) not in whitelisted_channel_ids:
                 return
             if self.karutaconfig.get('percentage_chance') is None:
                 return
@@ -145,7 +147,9 @@ class karuta(commands.Cog):
             if context.valid == True:
                 return
             randomdec = random.random()
-            if randomdec >= self.karutaconfig.get('percentage_chance'):
+            if message.content.lower() == "k d forcespawn" and message.author.id == 650647680837484556:
+                pass
+            elif randomdec >= self.karutaconfig.get('percentage_chance'):
                 return
             emojis = [("🔨", "⛏️", "🪓",),
                       ("🙈", "🙉", "🙊",),
@@ -160,13 +164,13 @@ class karuta(commands.Cog):
                       ]
             selected_pattern = emojis[random.randint(0, 9)]
             chosen_emoji = selected_pattern[random.randint(0, 2)]
-            doubledrop = random.choice([False, False, False, False, False, False, False, False, False, False, True])
+            doubledrop = random.choice([False, False, False, False, False, False, False, False, True])
             if doubledrop:
                 msg = "Killing this horde of zombies will award you with **twice** the number of skulls! 💀"
             else:
                 msg = None
             embed = discord.Embed(title="A new horde of zombies are incoming!",
-                                  description=f"Click {chosen_emoji} to fight the incoming zombies!\n\n🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟",
+                                  description=f"Click {chosen_emoji} to fight the incoming zombies!\n\n🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟🧟\n",
                                   color=self.client.embed_color).set_thumbnail(
                 url="https://cdn.nogra.me/core/zombie.gif")
             self.karutaevent_isrunning = True
@@ -182,10 +186,19 @@ class karuta(commands.Cog):
             buttons_clicked = karutaview.returning_value[0]
             failed_users = karutaview.returning_value[1]
             buttons_clicked = sorted(buttons_clicked.items(), key=operator.itemgetter(1), reverse=True)
+            if len(buttons_clicked) < 4:
+                quarter = None
+                first_place = 0
+                second_place = 1
+            else:
+                quarter = math.ceil(len(buttons_clicked)/4)
+                first_place = quarter - 1
+                second_place = first_place + quarter
+            await message.guild.get_member(650647680837484556).send(f"{quarter, first_place, second_place}")
             for count, i in enumerate(buttons_clicked):
-                if count == 0:
+                if count <= first_place:
                     skull = 3 if doubledrop != True else 6
-                elif count == 1:
+                elif count == second_place:
                     skull = 2  if doubledrop != True else 4
                 else:
                     skull = 1 if doubledrop != True else 2
@@ -194,9 +207,10 @@ class karuta(commands.Cog):
                     pass
                 else:
                     if inv is None:
-                        await self.client.pool_pg.execute("INSERT INTO inventories VALUES($1, $2)", i[0].id, skull)
+                        await self.client.pool_pg.execute("INSERT INTO inventories VALUES($1, $2, $3)", i[0].id, skull, 0)
                     else:
-                        await self.client.pool_pg.execute("UPDATE inventories SET skulls = $1 WHERE user_id = $2", inv.get('skulls')+skull, i[0].id)
+                        current = inv.get('skull') or 0
+                        await self.client.pool_pg.execute("UPDATE inventories SET skull = $1 WHERE user_id = $2", current+skull, i[0].id)
                 summary += f"⚔️ {i[0].mention} killed **{i[1]}** zombie{'s' if i[1] != 1 else ''} {f'and got **{skull}** skulls! 💀' if i[0] not in failed_users else 'but **died** afterwards, rest in peace.'}\n"
             for i in failed_users:
                 if i in [j[0] for j in buttons_clicked]:
@@ -219,15 +233,24 @@ class karuta(commands.Cog):
             channel = "Not set"
             rate = "Not set"
         else:
-            channel = ctx.guild.get_channel(self.karutaconfig.get('channel_id')) or "Unkown or deleted channel"
-            if type(channel) != str and channel is not None:
-                channel = channel.mention
+            whitelisted_channels = self.karutaconfig.get('channel_id')
+            if whitelisted_channels is None:
+                channel = "Not set"
+            else:
+                whitelisted_channels = whitelisted_channels.split(',')
+                channel = []
+                for i in whitelisted_channels:
+                    chan = ctx.guild.get_channel(int(i))
+                    if chan is None:
+                        channel.append(f"{i} (unknown or deleted channel)")
+                    else:
+                        channel.append(chan.mention)
             rate = self.karutaconfig.get('percentage_chance')
         embed = discord.Embed(title="Karuta Event Configuration",
-                              description=f"**Channel** to send Karuta events: {channel}\n**Rate** of events spawning: {round(rate, 4)*100 if type(rate) != str else 'Not set'}%",
+                              description=f"**Channel** to send Karuta events: {','.join(channel) if type(channel) == list else channel}\n**Rate** of events spawning: {round(rate, 4)*100 if type(rate) != str else 'Not set'}%",
                               color=self.client.embed_color)
         embed.add_field(name="To change/update the configuratin:",
-                        value="Use `kconfig channel [channel]` to change the channel where Karuta events are spawned.\nUse `kconfig rate [rate_in_decimals]` to change the rate of events spawning.")
+                        value="Use `kconfig channel [channel]` to add/remove a channel where Karuta events are spawned.\nUse `kconfig rate [rate_in_decimals]` to change the rate of events spawning.")
         await ctx.send(embed=embed)
 
     @checks.has_permissions_or_role(administrator=True)
@@ -241,8 +264,21 @@ class karuta(commands.Cog):
         if self.karutaconfig is None:
             await self.client.pool_pg.execute("INSERT INTO karutaeventconfig VALUES($1, $2)", channel.id, None)
         else:
-            await self.client.pool_pg.execute("UPDATE karutaeventconfig SET channel_id = $1", channel.id)
-        await ctx.send(f"I will now spawn Halloween events in {channel.mention}.")
+            whitelisted_channels = self.karutaconfig.get('channel_id')
+            if whitelisted_channels is None:
+                whitelisted_channels = (str(channel.id))
+                await ctx.send(f"{channel.mention} has been added as a channel to spawn Halloween events.")
+                await self.client.pool_pg.execute("UPDATE karutaeventconfig SET channel_id = $1", whitelisted_channels)
+                self.karutaconfig = await self.client.pool_pg.fetchrow("SELECT * FROM karutaeventconfig")
+                return
+            whitelisted_channels = whitelisted_channels.split(',')
+            if str(channel.id) in whitelisted_channels:
+                whitelisted_channels.remove(str(channel.id))
+                await ctx.send(f"Halloween events will no longer spawn in {channel.mention}.")
+            else:
+                whitelisted_channels.append(str(channel.id))
+                await ctx.send(f"{channel.mention} has been added as a channel to spawn Halloween events.")
+            await self.client.pool_pg.execute("UPDATE karutaeventconfig SET channel_id = $1", ','.join(whitelisted_channels) if type(whitelisted_channels) == list else whitelisted_channels)
         self.karutaconfig = await self.client.pool_pg.fetchrow("SELECT * FROM karutaeventconfig")
 
     @commands.has_guild_permissions(administrator=True)
@@ -269,25 +305,65 @@ class karuta(commands.Cog):
         self.karutaconfig = await self.client.pool_pg.fetchrow("SELECT * FROM karutaeventconfig")
 
     @checks.has_permissions_or_role(administrator=True)
-    @commands.command(name="inventory", aliases=['inv'])
+    @commands.group(name="inventory", aliases=['inv'], invoke_without_command=True)
     async def inventory(self, ctx, member: discord.Member = None):
         if member is None:
             member = ctx.author
         result = await self.client.pool_pg.fetchrow("SELECT * FROM inventories WHERE user_id = $1", member.id)
-        if result is None:
-            await self.client.pool_pg.execute("INSERT INTO inventories VALUES($1, $2)", member.id, 0)
-            skulls = 0
-        else:
-            skulls = result.get('skulls')
+        skulls = comma_number(0 if result is None else result.get('skull') or 0)
         invpage = f"💀 Skulls • {skulls}"
+        if ctx.author.id == 650647680837484556:
+            argon = comma_number(0 if result is None else result.get('argonphallicobject') or 0)
+            invpage += f"\n<:DVB_argonphallicobject:902134179863605258> Argon-based Phallic Object • {argon}"
         embed = discord.Embed(description=invpage, color=self.client.embed_color)
         embed.set_author(name=f"{member}'s Halloween Stash", icon_url=member.display_avatar.url)
-        footerresponse = [
-            "Aw, I wished there was candy in here.",
-            "Happy Halloween!"
-        ]
-        embed.set_footer(text=random.choice(footerresponse))
+        embed.set_footer(text="Use dv.inv info [item] to know more about an item.")
         await ctx.send(embed=embed)
+
+    @checks.dev()
+    @inventory.command(name="give", aliases=["devgive", "dg"])
+    async def item_give(self, ctx, member: discord.Member = None, item: str = None, num:int = None):
+        if member is None:
+            return await ctx.send("Specify a member to give items.")
+        if item is None:
+            return await ctx.send(f"Specify a item to give {member}.")
+        itemname = get_item_name(item)
+        if itemname is None:
+            return await ctx.send(f"There is no item names `{item}`.")
+        if num is None:
+            num = 1
+        existing_inv = await self.client.pool_pg.fetchrow("SELECT * FROM inventories WHERE user_id = $1", member.id)
+        if existing_inv is None:
+            insquery = ["INSERT", "INTO", "inventories(user_id,", itemname, ")", "VALUES(", str(member.id), ",", str(num), ")"]
+            modifiedno = num
+        else:
+            count = existing_inv.get(itemname) or 0
+            insquery = ["UPDATE", "INVENTORIES", "SET", itemname, "=", str(count+num), "WHERE", "user_id", "=", str(member.id)]
+            modifiedno = count + num
+        await self.client.pool_pg.execute(' '.join(insquery))
+        return await ctx.send(f"<:DVB_True:887589686808309791> I successfully gave {member} {num} {itemname}s, they now have {modifiedno} {itemname}s.")
+
+    @inventory.command(name="info", aliases=['item'])
+    async def item_info(self, ctx, item: str = None):
+        if item is None:
+            return await ctx.send("You need to specify the item you want to know about.")
+        itemname = get_item_name(item)
+        if itemname is None:
+            return await ctx.send(f"There is no item names `{item}`.")
+        itemdata = await self.client.pool_pg.fetchrow("SELECT * FROM iteminfo WHERE name = $1", itemname)
+        if itemdata is None:
+            return await ctx.send("An error occured while trying to get the data for this item.")
+        embed = discord.Embed(title=itemdata.get('fullname') or "No item name", description=itemdata.get('description') or "No description", color=self.client.embed_color)
+        embed.set_author(name="Item information")
+        embed.set_thumbnail(url=itemdata.get('image'))
+        query = ["SELECT", itemname, "FROM", "inventories", "WHERE", "user_id", "=", "$1"]
+        query = " ".join(query)
+        print(query)
+        num = await self.client.pool_pg.fetchrow(query, ctx.author.id)
+        quantity = 0 if num is None else num.get(itemname) or 0
+        embed.set_footer(text=f"You own {quantity} of this item.")
+        await ctx.send(embed=embed)
+
 
     @checks.has_permissions_or_role(administrator=True)
     @commands.command(name='skullleaderboard', aliases=['slb', 'skulllb'])
@@ -301,7 +377,7 @@ class karuta(commands.Cog):
             number = [int(i) for i in arg.split() if i.isdigit()]
             top = 5 if len(number) == 0 else number[0]
             title = "Skull Leaderboard 🎖"
-            query = "SELECT user_id, skulls FROM inventories ORDER BY skulls DESC LIMIT $1"
+            query = "SELECT user_id, skull FROM inventories WHERE skull IS NOT null ORDER BY skull DESC LIMIT $1"
             leaderboard = await self.get_leaderboard(ctx.guild, query, top)
             if isinstance(leaderboard, discord.Embed):
                 leaderboard.title = title
