5b91c1f9d5d862c3fbe12df1c556e1c8ed6d8d90
argo0n <laiyeqi@gmail.com>
2021-11-05 23:39:45 +0800

    freezenick, unfreezenick, roleinfo

--- cogs/mod/mod.py
+++ cogs/mod/mod.py
@@ -4,7 +4,7 @@ import datetime
 import asyncio
 from discord.ext import commands
 from utils import checks
-from utils.format import text_to_file
+from utils.format import text_to_file, stringtime_duration
 from .lockdown import lockdown
 from .censor import censor
 from utils.buttons import *
@@ -12,6 +12,10 @@ from .browser_screenshot import BrowserScreenshot
 from selenium import webdriver
 from collections import Counter
 import os
+from utils.time import humanize_timedelta
+import time
+from utils.converters import BetterRoles
+from fuzzywuzzy import process
 
 class Mod(censor, BrowserScreenshot, lockdown, commands.Cog, name='mod'):
     """
@@ -208,3 +212,119 @@ class Mod(censor, BrowserScreenshot, lockdown, commands.Cog, name='mod'):
             messages.extend(f'- **{author}**: {count}' for author, count in spammers)
 
         await ctx.send('\n'.join(messages), delete_after=10)
+
+    class Duration(commands.FlagConverter, case_insensitive=True, delimiter=' ', prefix='--'):
+        duration: Optional[str]
+
+    @checks.has_permissions_or_role(administrator=True)
+    @commands.command(name='freezenick', aliases=['fn'])
+    async def freezenick(self, ctx, member:discord.Member = None, *, nickname:str = None):
+        """
+        Freezes a user's nickname, causing their nickname to always display the nickname that you state in the command.
+        To specify a duration, add --duration [duration] at the end.
+        """
+        intduration = None
+        if member is None:
+            return await ctx.send("You need to tell me who you want to freezenick.")
+        if nickname is None:
+            return await ctx.send("You need to tell me what nickname you want to use.")
+        if len(nickname) > 32:
+            return await ctx.send("That nickname is too long, just like my-", delete_after=3.0)
+        existing = await self.client.pool_pg.fetchrow("SELECT * FROM freezenick WHERE user_id = $1 and guild_id = $2", member.id, ctx.guild.id)
+        if existing is not None:
+            return await ctx.send("A freezenick is already implemented for this user; the user most likely lost a nickbet.")
+        try:
+            old_nick = member.display_name
+            await member.edit(nick=nickname)
+        except:
+            return await ctx.send(f"I encountered an error while trying to freeze {member}'s nickname. It could be due to role hierachy or missing permissions.")
+        else:
+            timetounfreeze = 9223372036854775807
+            await self.client.pool_pg.execute("INSERT INTO freezenick(user_id, guild_id, nickname, old_nickname, time, reason) VALUES($1, $2, $3, $4, $5, $6)", member.id, ctx.guild.id, nickname, old_nick, timetounfreeze, f"Freezenick command invoked by {ctx.author}")
+            return await ctx.send(f"{member.mention}'s nickname is now frozen to `{nickname}`.")
+
+    @checks.has_permissions_or_role(administrator=True)
+    @commands.command(name='unfreezenick', aliases=['ufn'])
+    async def unfreezenick(self, ctx, member: discord.Member = None):
+        """
+        Unfreezes a user's nickname.
+        """
+        intduration = None
+        if member is None:
+            return await ctx.send("You need to tell me who you want to freezenick.")
+        existing = await self.client.pool_pg.fetchrow("SELECT * FROM freezenick WHERE user_id = $1 and guild_id = $2",
+                                                      member.id, ctx.guild.id)
+        if existing is None:
+            return await ctx.send(f"{member}'s nickname is currently not frozen.")
+        try:
+            await member.edit(nick=existing.get('old_nickname'))
+        except:
+            return await ctx.send(f"I encountered an error while trying to unfreeze {member}'s nickname. It could be due to role hierachy or missing permissions.")
+        else:
+            await self.client.pool_pg.execute("DELETE FROM freezenick WHERE id = $1", existing.get('id'))
+            return await ctx.send(f"{member.mention}'s nickname is now unfrozen.")
+
+    class BetterRoles(commands.Converter):
+        async def convert(self, ctx, argument):
+            try:
+                return await commands.RoleConverter().convert(ctx, argument)
+            except commands.BadArgument:
+                role_to_return = discord.utils.find(lambda x: x.name.lower() == argument.lower(), ctx.guild.roles)
+                if role_to_return is not None:
+                    return role_to_return
+                roles_and_aliases = {}
+                for r in ctx.guild.roles:
+                    roles_and_aliases[r.name] = r.id
+                    # This might be a bad idea, don't care
+                name, ratio = process.extractOne(argument, [x for x in roles_and_aliases])
+                if ratio >= 75:
+                    role_to_return = discord.utils.get(ctx.guild.roles, id=roles_and_aliases[name])
+                    return role_to_return
+
+    @checks.has_permissions_or_role(administrator=True)
+    @commands.command(name="roleinfo", aliases=['ri'])
+    async def roleinfo(self, ctx, *, role: BetterRoles = None):
+        if role is None:
+            return await ctx.send("What role do you want to know about?")
+        rolename = role.name
+        members = len(role.members)
+        color = format(role.color.value, 'x')
+        color = str(color)
+        while len(color) < 6:
+            color = f"0{color}"
+        color = f"#{color}"
+        hoisted = role.hoist
+        mentionable = role.mentionable
+        created = role.created_at
+        created = created.strftime("%d/%m/%Y %H:%M:%S")
+        desc = [f"Color: **{color}**", f"Hoisted: **{hoisted}**", f"Members with this role: **{members}**",
+                f"Mentionable: **{'<:DVB_True:887589686808309791>' if mentionable else '<:DVB_False:887589731515392000>'}**",
+                f"Created on: **{created}**", '']
+        if role.is_default():
+            desc.append("⚠️This is a guild-default role.")
+        elif role.is_bot_managed():
+            bot_id = role.tags.bot_id
+            desc.append(f"⚠️This role is an integration role for the bot {ctx.guild.get_member(bot_id)}.")
+        elif role.is_premium_subscriber():
+            desc.append("<a:DVB_Boost:906198274770346015> This role is the server's Booster role.")
+        elif role.managed or role.is_integration():
+            integration_id = role.tags.integration_id
+            integrations = await ctx.guild.integrations()
+            discIntegration = discord.utils.get(integrations, id=integration_id)
+            if discIntegration is None:
+                desc.append('⚠️This role is managed by an unknown integration.')
+            else:
+                desc.append(f"⚠️This role is managed by the integration {discIntegration.name} ({discIntegration.id}.")
+        if desc[-1] != '':
+            desc.append('')
+        position = role.position
+        if position == 0:
+            str_position=f"{ctx.guild.roles[2].name}\n{ctx.guild.roles[1].name}\nLowest role: **{role.name}**"
+        elif position+1 == len(ctx.guild.roles):
+            str_position=f"Highest role: **{role.name}**\n{ctx.guild.roles[-2].name}\n{ctx.guild.roles[-3].name}"
+        else:
+            str_position=f"{ctx.guild.roles[position-1].name}\n**{role.name}**\n{ctx.guild.roles[position+1].name}"
+        embed = discord.Embed(title=f"Role Info for {rolename}", description='\n'.join(desc), color=role.color)
+        embed.add_field(name="Positon", value=str_position, inline=False)
+        embed.set_footer(text=f"Role ID: {role.id}")
+        await ctx.send(embed=embed)
