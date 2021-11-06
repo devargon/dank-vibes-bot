4065ae49d793d9244efde72dc70ab4353ae09e2f
argo0n <laiyeqi@gmail.com>
2021-11-06 12:20:27 +0800

    checking_verifcation but sends a message instead of kicking

--- cogs/automod/verification.py
+++ cogs/automod/verification.py
@@ -1,9 +1,39 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from time import time
 
 class Verification(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @tasks.loop(minutes=30.0)
+    async def check_verification(self):
+        for guild in self.client.guilds:
+            is_enabled = await self.client.pool_pg.fetchval("SELECT enabled FROM serverconfig WHERE settings = $1 and guild_id = $2", 'verification', guild.id)
+            if is_enabled:
+                has_not_verified = [member for member in guild.members if (not member.bot) and (member.pending == True)]
+                str = ""
+                for member in has_not_verified:
+                    if time() - member.joined_at.timestamp() > 86400:
+                        if len(str) < 1800:
+                            str += f"{member.mention} has not verified **and is eligible to be kicked.**."
+                        else:
+                            await self.client.get_channel(616007729718231161).send(str)
+                    else:
+                        if len(str) < 1800:
+                            str += f"{member.mention} has not verified but joined less than 24 hours ago."
+                        else:
+                            await self.client.get_channel(616007729718231161).send(str)
+                await self.client.get_channel(616007729718231161).send(str)
+                """try:
+                    await member.send("You were automatically kicked from Dank Vibes as you have not agreed to the rules yet. To participate in the many giveaways/heists that Dank Vibes offers, come back and **agree to the rules** by pressing `Complete` at the bottom of your Discord client! https://cdn.discordapp.com/attachments/616007729718231161/906394155343818832/Screenshot_2021-11-06_at_9.36.42_AM.png\nhttps://discord.gg/dankmemer")
+                except:
+                    await self.client.get_channel(616007729718231161).send(f"I was unable to DM {member} that they were kicked for not completing the verification.")
+                await member.kick(reason="Auto kick - Incomplete Verification")"""
+
+
+
+
 
     @commands.Cog.listener()
     async def on_member_update(self, member_before, member_after):
