3da28923496e0a5a8bb8b59c7e360eca01e49c00
argo0n <laiyeqi@gmail.com>
2021-11-06 20:57:16 +0800

    change no tags message

--- cogs/fun/fun.py
+++ cogs/fun/fun.py
@@ -81,7 +81,7 @@ class Fun(games, ItemGames, snipe, imgen, dm, commands.Cog, name='fun'):
             return True
         else:
             if discord.utils.get(ctx.author.roles, name="No Tags"):
-                raise ArgumentBaseError(message="You have been stunned (or you're restricted from using Carl-bot tags) and can't use any commands at the moment. <:dv_pepeHahaUSuckOwO:837653798313918475>")
+                raise ArgumentBaseError(message="You have the **No Tags** role and can't use any commands in the **Fun** cantegory. <:dv_pepeHahaUSuckOwO:837653798313918475>")
         return True
 
     @checks.requires_roles()
