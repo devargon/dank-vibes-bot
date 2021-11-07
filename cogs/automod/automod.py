947b589a43228d8ce7a319a4ac1993bee6d9f56e
argo0n <laiyeqi@gmail.com>
2021-11-07 14:58:02 +0800

    automod stuffs

--- cogs/automod/automod.py
+++ cogs/automod/automod.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
 from .freezenick import Freezenick
-
-class AutoMod(Freezenick, commands.Cog):
+from .verification import Verification
+class AutoMod(Verification, Freezenick, commands.Cog):
     """
     This file is just a placeholder for the various automod functions/modules.
     """
     def __init__(self, client):
         self.client = client
         self.freezenick.start()
+        self.check_verification.start()
 
     def cog_unload(self) -> None:
         self.freezenick.stop()
+        self.check_verification.stop()
