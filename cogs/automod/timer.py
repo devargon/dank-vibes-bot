8b810e7d15e7cc07e738918de22faa54bd541121
argo0n <laiyeqi@gmail.com>
2022-01-13 23:39:42 +0800

    can this itmer thing fix itself pls

--- cogs/automod/timer.py
+++ cogs/automod/timer.py
@@ -21,47 +21,48 @@ class timer(commands.Cog):
                 if timer_record.get('time') < round(time()):
                     await self.client.pool_pg.execute("DELETE FROM timers WHERE message_id = $1", timer_record.get('message_id'))
             chan_id = timer_record.get('channel_id')
-            channel = self.client.get_channel(chan_id)
+            channel = guild.get_channel(int(chan_id))
             if channel is None:
                 if timer_record.get('time') < round(time()):
                     await self.client.pool_pg.execute("DELETE FROM timers WHERE message_id = $1", timer_record.get('message_id'))
-            message = channel.get_partial_message(timer_record.get('message_id'))
-            author = self.client.get_user(timer_record.get('user_id'))
-            title = timer_record.get('title')
-            if author is None:
-                if title is None:
-                    title = "Timer"
-                else:
-                    title = f"{title} Timer"
             else:
-                if title is None:
-                    title = f"{author.name}'s Timer"
-                else:
-                    title = f"{author.name}'s {title} Timer"
-            endtime = timer_record.get('time')
-            embed = discord.Embed(color=self.client.embed_color, timestamp=datetime.fromtimestamp(endtime)).set_author(name=title, icon_url=guild.icon.url)
-            embed.set_footer(text=f"Timer ends at")
-            if endtime < round(time()):
-                await self.client.pool_pg.execute("DELETE FROM timers WHERE message_id = $1", timer_record.get('message_id'))
-                embed.title="Timer is over! 🎊"
-                try:
-                    await message.edit(embed=embed)
-                except (discord.NotFound, discord.Forbidden, discord.HTTPException):
-                    pass
-                try:
-                    usermention = author.mention if author is not None else None
-                    if usermention is None:
-                        await channel.send(f"The {title} is over! 🎊")
+                message = channel.get_partial_message(timer_record.get('message_id'))
+                author = self.client.get_user(timer_record.get('user_id'))
+                title = timer_record.get('title')
+                if author is None:
+                    if title is None:
+                        title = "Timer"
                     else:
-                        await channel.send(f"{usermention}'s {title} is over! 🎊")
-                except (discord.NotFound, discord.Forbidden, discord.HTTPException):
-                    pass
-            else:
-                embed.title = f"{humanize_timedelta(seconds=endtime - round(time()))} remaining..."
-                try:
-                    await message.edit(embed=embed)
-                except (discord.NotFound, discord.Forbidden, discord.HTTPException):
-                    pass
+                        title = f"{title} Timer"
+                else:
+                    if title is None:
+                        title = f"{author.name}'s Timer"
+                    else:
+                        title = f"{author.name}'s {title} Timer"
+                endtime = timer_record.get('time')
+                embed = discord.Embed(color=self.client.embed_color, timestamp=datetime.fromtimestamp(endtime)).set_author(name=title, icon_url=guild.icon.url)
+                embed.set_footer(text=f"Timer ends at")
+                if endtime < round(time()):
+                    await self.client.pool_pg.execute("DELETE FROM timers WHERE message_id = $1", timer_record.get('message_id'))
+                    embed.title="Timer is over! 🎊"
+                    try:
+                        await message.edit(embed=embed)
+                    except (discord.NotFound, discord.Forbidden, discord.HTTPException):
+                        pass
+                    try:
+                        usermention = author.mention if author is not None else None
+                        if usermention is None:
+                            await channel.send(f"The {title} is over! 🎊")
+                        else:
+                            await channel.send(f"{usermention}'s {title} is over! 🎊")
+                    except (discord.NotFound, discord.Forbidden, discord.HTTPException):
+                        pass
+                else:
+                    embed.title = f"{humanize_timedelta(seconds=endtime - round(time()))} remaining..."
+                    try:
+                        await message.edit(embed=embed)
+                    except (discord.NotFound, discord.Forbidden, discord.HTTPException):
+                        pass
 
     @timer_loop.before_loop
     async def before_timer_loop(self):
