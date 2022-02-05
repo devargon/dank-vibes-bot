b4bec0d209d7d88205eddc2a57ffbf3afe62154c
argo0n <laiyeqi@gmail.com>
2022-02-05 23:22:35 +0800

    role rall command

--- cogs/mod/role.py
+++ cogs/mod/role.py
@@ -1,14 +1,38 @@
+import math
+
 import discord
 from discord.ext import commands
+from fuzzywuzzy import process
 
 from utils import checks
-from utils.converters import BetterRoles
 
 import imghdr
 import aiohttp
 from typing import Union
 from emoji import UNICODE_EMOJI
 import re
+
+from utils.buttons import confirm
+from utils.format import generate_loadbar
+from utils.time import humanize_timedelta
+
+
+class BetterRoles(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            return await commands.RoleConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            role_to_return = discord.utils.find(lambda x: x.name.lower() == argument.lower(), ctx.guild.roles)
+            if role_to_return is not None:
+                return role_to_return
+            roles_and_aliases = {}
+            for r in ctx.guild.roles:
+                roles_and_aliases[r.name] = r.id
+                # This might be a bad idea, don't care
+            name, ratio = process.extractOne(argument, [x for x in roles_and_aliases])
+            if ratio >= 75:
+                role_to_return = discord.utils.get(ctx.guild.roles, id=roles_and_aliases[name])
+                return role_to_return
 
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
@@ -23,7 +47,7 @@ class Role(commands.Cog):
 
     @checks.has_permissions_or_role(manage_roles=True)
     @commands.group(name="role", invoke_without_command=True)
-    async def role_base(self, ctx, member: discord.Member = None, *, role: BetterRoles = None):
+    async def role_cmd(self, ctx, member: discord.Member = None, *, role: BetterRoles = None):
         """
         Use this command to add or remove a role to a user.
         """
@@ -51,8 +75,8 @@ class Role(commands.Cog):
             await ctx.send(f"Added **{role.name}** to **{member}**.")
 
     @checks.has_permissions_or_role(manage_roles=True)
-    @role_base.command(name='icon')
-    async def role_icon(self, ctx, role: discord.Role = None, argument: Union[discord.Emoji, discord.PartialEmoji, str] = None):
+    @role_cmd.command(name='icon')
+    async def role_icon(self, ctx, role: BetterRoles = None, argument: Union[discord.Emoji, discord.PartialEmoji, str] = None):
         """
         Changes the icon of a role in the server.
         The supported arguments are an emoji, a image URL, or an attachment.
@@ -138,3 +162,54 @@ class Role(commands.Cog):
                 raise e
             elif e == discord.InvalidArgument:
                 return await ctx.send("The role you provided is not valid.")
+
+    @checks.has_permissions_or_role(manage_roles=True)
+    @role_cmd.command(name="removeall", aliases=['rall'])
+    async def role_removeall(self, ctx, *, role: BetterRoles = None):
+        """Removes all members from a role."""
+        if role is None:
+            return await ctx.send("You need to provide a role.")
+        role: discord.Role = role
+        if len(role.members) == 0:
+            return await ctx.send(f"There is no one with the role **{role.name}**!")
+        if len(role.members) < 5:
+            approx = 1.0
+        else:
+            approx = (1.0 +4.8) * (len(role.members)/5)
+        approx = round(approx)
+        confirmview = confirm(ctx, self.client, 10.0)
+        embed = discord.Embed(title="Dangerous action!", description=f"Are you sure you want to **remove** the **{role.name}** role off **{len(role.members)}** members? This process will take approximately **{humanize_timedelta(seconds=approx)}**.", color=discord.Color.orange())
+        confirmview.response = await ctx.send(embed=embed, view=confirmview)
+        await confirmview.wait()
+        if confirmview.returning_value is not True:
+            embed.color, embed.description = discord.Color.red(), "The action has been cancelled."
+            return await confirmview.response.edit(embed=embed)
+        success = 0
+        original_count = len(role.members)
+        failed = {}
+        split = math.ceil(len(role.members)/10) if len(role.members) > 30 else 1
+        def generate_embed():
+            embed = discord.Embed(title="Removing roles...", description=f"{success} of {original_count} members processed.", color=discord.Color.green())
+            embed.add_field(name="Progress", value=f"{generate_loadbar(percentage=success/original_count, length=10)} {round(success/original_count*100)}%", inline=False)
+            return embed
+        await confirmview.response.edit(embed=generate_embed())
+        for member in role.members:
+            try:
+                await member.remove_roles(role, reason=f"Requested by {ctx.author} ({ctx.author.id})")
+            except Exception as e:
+                failed[member] = str(e)
+            else:
+                success += 1
+            if success % split == 0:
+                embed = generate_embed()
+                await confirmview.response.edit(embed=embed)
+        cont = f"<:DVB_True:887589686808309791> **{role.name}** successfully removed from **{success}** members!"
+        if len(failed) > 0:
+            str_list = []
+            for member, error in failed.items():
+                str_list.append(f"{member} ({member.id}): {error}")
+            embed = discord.Embed(title=f"I couldn't remove the role from {len(failed)} people:", description="\n".join(str_list), color=discord.Color.red())
+        else:
+            embed = None
+        await confirmview.response.edit(embed=generate_embed())
+        await ctx.send(cont, embed=embed)
