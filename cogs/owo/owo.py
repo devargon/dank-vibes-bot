e87242df92b15732a456348ce7be1d5d261d5a58
argo0n <67531837+argo0n@users.noreply.github.com>
2022-02-06 12:43:07 +0800

    Change ID

--- cogs/owo/owo.py
+++ cogs/owo/owo.py
@@ -162,9 +162,10 @@ class OwO(commands.Cog, name='owo'):
                 query = "SELECT member_id, weekly_count FROM owocount ORDER BY weekly_count DESC LIMIT $1"
                 embed = await self.get_leaderboard(guild, query, top=5)
                 embed.title = "This week's OwO leaderboard"
-                if channel is not None:
+                ww_chan = guild.get_channel(937236577673945109)
+                if ww_chan is not None:
                     with contextlib.suppress(discord.HTTPException):
-                        await channel.send(embed=embed)
+                        await ww_chan.send(embed=embed)
             weekly_res = await self.client.pool_pg.fetch("SELECT member_id, weekly_count FROM owocount")
             reset_values = []
             for res in weekly_res:
