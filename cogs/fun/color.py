17c504379ae96ecad035c0f2abaf16ae82ddad57
argo0n <laiyeqi@gmail.com>
2022-02-06 16:34:50 +0800

    support a single color argument

--- cogs/fun/color.py
+++ cogs/fun/color.py
@@ -37,7 +37,7 @@ class color(commands.Cog):
     @checks.has_permissions_or_role(manage_roles=True)
     @commands.command(name="color", aliases=["colour"])
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def color(self, ctx, argument:Union[discord.Member, str] = None):
+    async def color(self, ctx, argument:Union[discord.Member, discord.Colour, str] = None):
         """
         Gets dominant and matching colors of your profile picture.
         """
@@ -46,24 +46,47 @@ class color(commands.Cog):
             image = await ctx.author.display_avatar.with_format('png').read()
         elif isinstance(argument, discord.Member):
             image = await argument.display_avatar.with_format('png').read()
+        elif isinstance(argument, discord.Colour):
+            image = argument
         elif isinstance(argument, str):
-            if not regex.match(argument):
-                await ctx.send("You provided an invalid image URL.")
-                return
-            image = await http.get(argument, res_method="read")
+            if len(argument) == 6:
+                try:
+                    argument = await commands.ColorConverter().convert(ctx, f"#{argument}")
+                except Exception as e:
+                    if not regex.match(argument):
+                        await ctx.send("You provided an invalid image URL.")
+                        return
+                    image = await http.get(argument, res_method="read")
+                else:
+                    image = argument
+            else:
+                if not regex.match(argument):
+                    await ctx.send("You provided an invalid image URL.")
+                    return
+                image = await http.get(argument, res_method="read")
         else:
             return await ctx.send('uhm')
-        try:
-            color_thief = ColorThief(BytesIO(image))
-        except UnidentifiedImageError:
-            return await ctx.send("I could not read the image provided.")
-        palette = color_thief.get_palette(color_count=6)
-        messagecontents = []
-        for color in palette:
+        if isinstance(image, discord.Colour):
+            color = image.to_rgb()
             hexcode = rgb_to_hex(color)
             hex_int = int(hexcode, 16)
             hex_int = hex_int + 0x200
-            messagecontents.append((f"HEX: `{hexcode}`\nRGB: `{color}`\nINT: `{int(hex_int)}`", f"https://api.alexflipnote.dev/color/image/{hexcode}", int(hex_int)))
-        title = f"{argument.name}'s Profile Picture Color" if isinstance(argument, discord.Member) else "Image dominant colors"
-        pages = CustomMenu(source=ColorDisplay(messagecontents, title, argument.display_avatar.url if isinstance(argument, discord.Member) else argument), clear_reactions_after=True, timeout=60)
+            messagecontents = (f"HEX: `{hexcode}`\nRGB: `{color}`\nINT: `{int(hex_int)}`", f"https://api.alexflipnote.dev/color/image/{hexcode}", int(hex_int))
+        else:
+            try:
+                color_thief = ColorThief(BytesIO(image))
+            except UnidentifiedImageError:
+                return await ctx.send("I could not read the image provided.")
+            palette = color_thief.get_palette(color_count=6)
+            messagecontents = []
+            for color in palette:
+                hexcode = rgb_to_hex(color)
+                hex_int = int(hexcode, 16)
+                hex_int = hex_int + 0x200
+                messagecontents.append((f"HEX: `{hexcode}`\nRGB: `{color}`\nINT: `{int(hex_int)}`", f"https://api.alexflipnote.dev/color/image/{hexcode}", int(hex_int)))
+        title = f"{argument.name}'s Profile Picture Color" if isinstance(argument, discord.Member) else "Your Color" if isinstance(argument, discord.Color) else "Image dominant colors"
+        if type(messagecontents) == tuple:
+            pages = CustomMenu(source=ColorDisplay([messagecontents], title, messagecontents[1]), clear_reactions_after=True, timeout=60)
+        else:
+            pages = CustomMenu(source=ColorDisplay(messagecontents, title, argument.display_avatar.url if isinstance(argument, discord.Member) else argument), clear_reactions_after=True, timeout=60)
         return await pages.start(ctx)
