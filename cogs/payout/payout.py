2ad570b0173604580267142e0dfb4aff2816a169
argo0n <argon@nogra.xyz>
2022-07-13 15:48:22 +0800

    this is really weird

--- cogs/payout/payout.py
+++ cogs/payout/payout.py
@@ -57,7 +57,7 @@ class PayoutManagement(commands.Cog):
             else:
                 row = await self.client.db.fetchrow("SELECT * FROM payoutchannels WHERE channel_id = $1", message.channel.id)
                 if row is not None and message.author.id != row.get('user_id'):
-                    if (discord.utils.get(message.author.roles, id=608495204399448066) is not None or discord.utils.get(message.author.roles, id=608500355973644299)):
+                    if discord.utils.get(message.author.roles, id=608495204399448066) is not None or discord.utils.get(message.author.roles, id=608500355973644299) is not None:
                         await self.client.db.execute("UPDATE payoutchannels SET staff = $1 WHERE channel_id = $2", message.author.id, message.channel.id)
                 else:
                     await self.client.db.execute("INSERT INTO payoutchannels(channel_id, staff) VALUES($1, $2)", message.channel.id, message.author.id)
