be438c80b4ef4db02d7ad3b63e945edad9d0aaf6
argo0n <argon@nogra.xyz>
2022-07-21 22:35:48 +0800

    handle autoban and decancering in member_join

--- cogs/events/on_ready.py
+++ cogs/events/on_ready.py
@@ -2,8 +2,6 @@ import os
 
 import discord
 from discord.ext import commands
-from .status_utils import check_status, get_custom_activity
-from .status_task import check_status
 from main import dvvt
 
 class Ready(commands.Cog):
