374205af8e0dd38c31c6a12b64ab8885ee030f37
argo0n <argon@nogra.xyz>
2022-08-24 01:40:41 +0800

    remove print statements

--- cogs/events/on_message.py
+++ cogs/events/on_message.py
@@ -197,7 +197,7 @@ class OnMessage(commands.Cog):
                             await message.channel.send(f"**Robbing is disabled** in {message.guild.name}. This is for the safety of everyone's wallets in this server.")
                         else:
                             last_sent = self.pls_prompt.get(message.channel.id, 0)
-                            if round(time()) - last_sent >= 60:
+                            if round(time()) - last_sent >= 120:
                                 if message.channel.category_id in [644006682392788992, 797167555767697408, 947298067500114061, 608505446717849630, 802467427208265728, 763457841133912074, 789195494664306688, 783299769580781588, 805052824185733120, 834696686923284510, 847897065081274409, 875316745416617984]:
                                     embed = discord.Embed(title="Dank Memer has switched to slash commands.", description="**`pls` commands have been replaced** with <:DVB_commands:913426937869926440> **slash commands.**\nType `/` to find your favorite Dank Memer command.\n\n`pls help` → `/help`\n`pls beg` -> `/beg`\n\nIf you have answers or queries, join [Dank Memer's Support Server.](https://discord.gg/dankmemerbot)", color=0xff0000)
                                     embed.set_footer(text=message.guild.name, icon_url=message.guild.icon.url)
@@ -205,12 +205,6 @@ class OnMessage(commands.Cog):
                                     if not message.channel.permissions_for(message.author).manage_messages:
                                         await message.channel.send(content=message.author.mention, embed=embed)
                                         self.pls_prompt[message.channel.id] = round(time())
-                                    else:
-                                        print(f"{message.channel.name}:{message.author} | user has manage message perms")
-                                else:
-                                    print(f"{message.channel.name}:{message.author} | not in appropraite category")
-                            else:
-                                print(f"{message.channel.name}:{message.author} | current time {round(time())} is not 60 more than {last_sent}")
             if settings.mrob_ar is True:
                 if con.startswith('m.rob') or con.startswith('m.steal'):
                     split_cmd = con.split(' ')
