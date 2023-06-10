07402b7f834bf15416ac8094f21a95db7e3ad1c0
Argon <argon@nogra.xyz>
2023-06-10 16:50:19 +0800

    add blob to giveaway embed

--- cogs/giveaways/giveaways.py
+++ cogs/giveaways/giveaways.py
@@ -659,7 +659,7 @@ class GiveawayView(discord.ui.View):
                         elif donator100m():
                             remarks.append(f"You can get <@&769491608189927434> by donating **⏣ 100,000,000** worth of Dank Memer items/coins in <#652729093649072168>, <#786944439360290826>, or <#722874833540481054>.")
                         elif gen_chat_active():
-                            remarks.append(f"You can get <@&899185283881381889> by chatting in <#608498967474601995> and reaching **150 messages sent** in `dv.mymessages`.")
+                            remarks.append(f"You can get <@&899185283881381889> by chatting in <#608498967474601995> and reaching **100 messages sent** in `dv.mymessages`.")
                         elif elite_giveaway_ping():
                             remarks.append(f"You can get <@&758174135276142593> by running `-rank elite giveaway ping` in <#698462922682138654>, or selecting the **Elite Giveaway Ping** role under **Dank Pings** in <#782586550486695936>.")
                         if gway_only_requires_voting():
@@ -1205,6 +1205,8 @@ class giveaways(commands.Cog):
             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/920812196567597067.webp?quality=lossless")
         elif "cursed" in title_lower:
             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/933194488241864704.webp?quality=lossless")
+        elif "blob" in title_lower:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/933188762782011454.gif?quality=lossless")
         else:
             guild = self.client.get_guild(entry.guild_id)
             if guild is not None and guild.icon is not None:
