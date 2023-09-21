3bb4f81fb4adb78c4d0a7962e371ebbaf3d0fc14
Argon <argon@nogra.xyz>
2023-09-21 16:52:00 +0800

    Update time display

--- cogs/utility/time.py
+++ cogs/utility/time.py
@@ -1,6 +1,7 @@
 import random
 from datetime import datetime
 
+import alexflipnote
 import amari
 import discord
 import pytz
@@ -14,6 +15,22 @@ from utils.time import humanize_timedelta
 from utils.format import comma_number
 from time import time
 from main import dvvt
+
+def get_time_color(hour):
+    if 0 <= hour < 4:
+        return 0x002244  # Deep night
+    elif 4 <= hour < 6:
+        return 0x004488  # Approaching sunrise
+    elif 6 <= hour < 8:
+        return 0xFFA500  # Sunrise
+    elif 8 <= hour < 16:
+        return 0x87CEEB  # Daytime
+    elif 16 <= hour < 18:
+        return 0xFF4500  # Approaching sunset
+    elif 18 <= hour < 20:
+        return 0x004488  # Just after sunset
+    else:
+        return 0x002244  # Deep evening into night
 
 
 async def timezone_autocomplete(ctx: discord.AutocompleteContext):
@@ -43,8 +60,12 @@ timezone_option = discord.Option(description="Select a timezone.", autocomplete=
 class UserTime(commands.Cog):
     def __init__(self, client):
         self.client: dvvt = client
+        self.alex_api = alexflipnote.Client()
 
     async def get_time_response(self, ctx, selected_member):
+        content = None
+        embeds = []
+        files = []
 
         def generate_time_disp(timezone):
             local_time = discord.utils.utcnow().astimezone(pytz.timezone(timezone))
@@ -60,43 +81,59 @@ class UserTime(commands.Cog):
             offset_str = f"UTC {'+' if hours > 0 else '-'} {abs(int(hours)):02}:{abs(int(minutes)):02}"
             return f"{date_str}, **{time_str}** ({offset_str})"
 
+        async def generate_time_embed(timezone, user: discord.Member):
+            local_time = discord.utils.utcnow().astimezone(pytz.timezone(timezone))
+
+
+            embed = discord.Embed(title=generate_time_disp(timezone), color=get_time_color(local_time.hour))
+            embed.set_footer(text=timezone)
+            embed.set_thumbnail(url=f"attachment://{user.id}.png")
+            embed.set_author(name=proper_userf(user), icon_url=user.display_avatar)
+            return embed
+
+
+
         ctx_userinfo = await self.client.fetch_user_info(ctx.author.id)
         ctx_user_timezone = ctx_userinfo.timezone
 
         if ctx_user_timezone is not None:
-            ctx_user_time_disp = generate_time_disp(ctx_user_timezone)
+            ctx_user_time = discord.utils.utcnow().astimezone(pytz.timezone(ctx_user_timezone))
+            img = await self.alex_api.clock_by_datetime(ctx_user_time)
+            img_bytes = await img.read()
+            files.append(discord.File(fp=img_bytes, filename=f"{ctx.author.id}.png"))
 
         tgt_user_timezone = None
         if selected_member is not None:
             selected_userinfo = await self.client.fetch_user_info(selected_member.id)
             tgt_user_timezone = selected_userinfo.timezone
             if tgt_user_timezone is not None:
-                tgt_user_time_disp = generate_time_disp(tgt_user_timezone)
+                tgt_user_time = discord.utils.utcnow().astimezone(pytz.timezone(tgt_user_timezone))
+                img = await self.alex_api.clock_by_datetime(tgt_user_time)
+                img_bytes = await img.read()
+                files.append(discord.File(fp=img_bytes, filename=f"{selected_member.id}.png"))
+
+
 
 
 
         if ctx_user_timezone is None:
             if selected_member is None:
-                return "You have not set a timezone. Run </time set:1144153317593854023> to set it."
+                content = "You have not set a timezone. Run </time set:1144153317593854023> to set it."
             else:
                 if tgt_user_timezone is None:
-                    return f"Both you and **{selected_member.name}** have not set a timezone."
+                    content = f"Both you and **{selected_member.name}** have not set a timezone."
                 else:
-                    return f"**{selected_member.name}**'s current timezone is **{tgt_user_timezone}**, the time there is {tgt_user_time_disp}. You have not set a timezone."
+                    embeds.append(await generate_time_embed(tgt_user_timezone, selected_member))
 
         if ctx_user_timezone is not None:
-            # Getting the current local time in the specified timezone
-
-
-            if selected_member is None:
-                return f"Your current timezone is **{ctx_user_timezone}**.\nThe current time is {ctx_user_time_disp}."
-            else:
+            embeds.append(await generate_time_embed(ctx_user_timezone, ctx.author))
+            if selected_member is not None:
                 if tgt_user_timezone is not None:
-                    tgt_local_time = discord.utils.utcnow().astimezone(pytz.timezone(tgt_user_timezone))
-                    local_time = discord.utils.utcnow().astimezone(pytz.timezone(ctx_user_timezone))
+                    embeds.append(await generate_time_embed(tgt_user_timezone, selected_member))
 
-                    tgt_offset_seconds = tgt_local_time.utcoffset().total_seconds()
-                    ctx_offset_seconds = local_time.utcoffset().total_seconds()
+
+                    tgt_offset_seconds = tgt_user_time.utcoffset().total_seconds()
+                    ctx_offset_seconds = ctx_user_time.utcoffset().total_seconds()
 
                     # Find the difference in hours
                     time_difference = (tgt_offset_seconds - ctx_offset_seconds) / 3600
@@ -108,30 +145,27 @@ class UserTime(commands.Cog):
                     else:
                         diff_str = f"You and **{selected_member.name}** are in the same timezone."
 
-                    return (
-                        f"Your current timezone is **{ctx_user_timezone}**, the time is {ctx_user_time_disp}.\n"
-                        f"**{selected_member.name}**'s current timezone is **{tgt_user_timezone}** and the time there is {tgt_user_time_disp}.\n{diff_str}")
+                    embeds.append(discord.Embed(description=diff_str, color=2829627))
                 else:
-                    return f"Your current timezone is **{ctx_user_timezone}**, the time is {ctx_user_time_disp}.\n**{selected_member.name}** has not set a timezone."
-
-        return "An unexpected error occurred."  # Fallback response
+                    content = f"**{selected_member.name}** has not set a timezone."
+        print("what")
+        return content, embeds, files
 
     @commands.guild_only()
     @commands.group(name='time')
-    async def time(self, ctx, *, member: discord.Member = None):
+    async def time_g(self, ctx, *, member: discord.Member = None):
         """
         Show yours or another user's time.
         """
-        c = await self.get_time_response(ctx, member)
-        await ctx.send(c)
-
+        c, es, files = await self.get_time_response(ctx, member)
+        await ctx.send(c, embeds=es, files=files)
 
     time_group = SlashCommandGroup("time", "Configure your timezone and show your time to others")
 
     @time_group.command(name="show", description="Show your current time or another user's time.")
     async def time_show_slash(self, ctx, member: discord.Option(discord.Member, description="Pick an optional member to see their time.", required=False)):
-        c = await self.get_time_response(ctx, member)
-        await ctx.respond(c)
+        c, es, files = await self.get_time_response(ctx, member)
+        await ctx.respond(c, embeds=es, files=files)
 
     @time_group.command(name="set", description="Set your timezone for us to show your current time.")
     async def time_set_slash(self, ctx: discord.ApplicationContext, zone: timezone_option):
