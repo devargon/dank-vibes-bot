834b0be475abe564e217805b379ab4f0632b56b5
Argon <argon@nogra.xyz>
2024-01-28 16:16:44 +0800

    dynamic IDs

--- cogs/banappeal/banappealcog.py
+++ cogs/banappeal/banappealcog.py
@@ -11,7 +11,7 @@ import typing
 import server
 from aiohttp import ClientSession
 
-from .banappeal_views import BanAppealView
+from .banappeal_views import BanAppealView, GetBannedView
 from .banappealdb import BanAppealDB, BanAppeal
 from main import dvvt
 from utils.format import proper_userf, print_exception
@@ -21,8 +21,9 @@ from discord.ext import commands, tasks
 from .banappeal_discord import BanAppealDiscord
 from .banappeal_server import BanAppealServer
 
-server_id = 871734809154707467
-banappeal_chn_id = 1194673636196491396
+server_id = 1200400184748802168 if "preproduction" in os.getenv("APPEALS_SERVER_HOST") else 595457764935991326 if "banappeal." in os.getenv("APPEALS_SERVER_HOST") else 871734809154707467
+banappeal_chn_id = 1200705116857176135 if "preproduction" in os.getenv("APPEALS_SERVER_HOST") else 679122147553312778 if "banappeal." in os.getenv("APPEALS_SERVER_HOST") else 1194673636196491396
+modlog_chn_id = 1200707746622869535 if "preproduction" in os.getenv("APPEALS_SERVER_HOST") else 640029959213285387 if "banappeal." in os.getenv("APPEALS_SERVER_HOST") else 999661054067998720
 
 class BanAppealCog(BanAppealServer, BanAppealDiscord, commands.Cog, name='banappeal'):
     """
@@ -119,6 +120,7 @@ class BanAppealCog(BanAppealServer, BanAppealDiscord, commands.Cog, name='banapp
         self.check_unupdated_appeals.start()
         self.check_appeal_deadlines.start()
         self.client.add_view(view=BanAppealView())
+        self.client.add_view(view=GetBannedView())
 
     @tasks.loop(seconds=5)
     async def check_unposted_appeals(self):
