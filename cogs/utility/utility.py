38ee5bf518c279647834fb9a3e338f2168c98c96
Argon <argon@nogra.xyz>
2024-02-03 11:33:26 +0800

    Update status link

--- cogs/utility/utility.py
+++ cogs/utility/utility.py
@@ -453,7 +453,7 @@ class Utility(UserTime, CustomRoleManagement, UtilitySlash, reminders, Highlight
         embed.add_field(name='Versions', value=f"<:DVB_python:955345550193078272> `{py_version}`\n<:DVB_PyCord:937351289514385428> `{dpy_version}`", inline=True)
         embed.add_field(name='Developers', value=f"{str(self.client.get_user(650647680837484556))}", inline=True)
         embed.add_field(name="Thanks To", value=f"{str(await self.client.fetch_user(727498137232736306))}\n{self.client.get_user(321892489470410763)}", inline=True)
-        embed.add_field(name="Important Links", value="[Status Page](http://status.dvbot.nogra.xyz/)\n[Terms of Service](https://docs.dvbot.nogra.xyz/legal/terms/) and [Privacy Policy](https://docs.dvbot.nogra.xyz/legal/privacy/)", inline=False)
+        embed.add_field(name="Important Links", value="[Status Page](https://status.nogra.xyz/status/dv)\n[Terms of Service](https://docs.dvbot.nogra.xyz/legal/terms/) and [Privacy Policy](https://docs.dvbot.nogra.xyz/legal/privacy/)", inline=False)
         if ctx.author.id in [650647680837484556, 515725341910892555, 321892489470410763]:
             loop = asyncio.get_event_loop()
             def get_advanced_details():
