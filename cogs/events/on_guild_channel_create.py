7f84d9debae36b36d7403268614addbda026d461
Argon <argon@nogra.xyz>
2024-12-16 10:38:50 +0800

    Fix things

--- cogs/events/on_guild_channel_create.py
+++ cogs/events/on_guild_channel_create.py
@@ -14,6 +14,9 @@ class GuildChannelCreate(commands.Cog):
         if isinstance(channel, discord.TextChannel):
             try:
                 def check(message: discord.Message):
+                    print(f"message.channel.id == channel.id: {message.channel.id == channel.id}")
+                    print(f"len(m.mentions) > 0: {len(m.mentions) > 0}")
+                    print(f"DVB_PVC_CREATED in message.content: " + "DVB_PVC_CREATED" in message.content)
                     return message.channel.id == channel.id and len(m.mentions) > 0 and "DVB_PVC_CREATED" in message.content
                 m = await self.client.wait_for('message', check=check, timeout=60)
             except asyncio.TimeoutError:
@@ -23,6 +26,7 @@ class GuildChannelCreate(commands.Cog):
                 if type(channel) == discord.TextChannel and channel.category is not None and "private channels" in channel.category.name.lower():
                     active = True
                 else:
+                    print(f"{type(channel) == discord.TextChannel} and {channel.category is not None} and " + "private channels" in channel.category.name.lower())
                     active = False
                 await self.client.db.execute("INSERT INTO channels(guild_id, channel_id, owner_id, active, last_used) VALUES($1, $2, $3, $4, $5)", channel.guild.id, channel.id, owner.id, active, round(time()))
                 if active:
