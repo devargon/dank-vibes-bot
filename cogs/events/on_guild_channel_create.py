587b531603cd35b43a301efe0b5062b9cc8a93ac
Argon <argon@nogra.xyz>
2024-12-16 10:20:10 +0800

    Fix pvc creation detection

--- cogs/events/on_guild_channel_create.py
+++ cogs/events/on_guild_channel_create.py
@@ -13,12 +13,17 @@ class GuildChannelCreate(commands.Cog):
     async def on_guild_channel_create(self, channel: discord.abc.GuildChannel):
         if isinstance(channel, discord.TextChannel):
             try:
-                m = await self.client.wait_for('message', check=lambda m: m.channel.id == channel.id and len(m.mentions) > 0, timeout=60)
+                def check(message: discord.Message):
+                    return message.channel.id == channel.id and len(m.mentions) > 0 and "DVB_PVC_CREATED" in message.content
+                m = await self.client.wait_for('message', check=check, timeout=60)
             except asyncio.TimeoutError:
                 pass
             else:
-                if type(channel) == discord.TextChannel and channel.category is not None and "vip channels" in channel.category.name.lower():
+                owner = m.mentions[0]
+                if type(channel) == discord.TextChannel and channel.category is not None and "private channels" in channel.category.name.lower():
                     active = True
                 else:
                     active = False
-                await self.client.db.execute("INSERT INTO channels(guild_id, channel_id, owner_id, active, last_used) VALUES($1, $2, $3, $4, $5)", channel.guild.id, channel.id, m.mentions[0].id, active, round(time()))
+                await self.client.db.execute("INSERT INTO channels(guild_id, channel_id, owner_id, active, last_used) VALUES($1, $2, $3, $4, $5)", channel.guild.id, channel.id, owner.id, active, round(time()))
+                if active:
+                    await channel.send(f"{owner.mention}, manage your channel with these commands: https://staticx.gh.nogra.app/dankvibesbot/privchannel/input-suggestion.png")
