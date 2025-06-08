29666432b2a761cc9c0ad265ae2bb379263e9a90
Argon <argon@nogra.app>
2025-06-08 10:18:57 +0800

    Store token inside worker record

--- table_schemas/096_amari_import_workers.sql
+++ table_schemas/096_amari_import_workers.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS amari_import_workers
 (
     id SERIAL,
     host TEXT,
+    token TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     worker_user_id BIGINT NOT NULL,
     creator_user_id BIGINT NOT NULL
