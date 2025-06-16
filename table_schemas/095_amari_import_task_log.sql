c7afac4b29ce457ebf880555e9d79d604ebea0ae
Argon <argon@nogra.xyz>
2025-06-16 18:21:35 +0800

    Change to use timestamptz

--- table_schemas/095_amari_import_task_log.sql
+++ table_schemas/095_amari_import_task_log.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS amari_import_task_log
 (
     id SERIAL,
     task_id INT NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     status_before TEXT NOT NULL,
     status_after TEXT NOT NULL,
     event TEXT NOT NULL,
